b4491b6609e330e8069ea6c1802ae8664b6791d5
Troy Stribling <troy.stribling@gmail.com>
2009-03-13 00:34:21 -0400

    starting work on parser

--- lib/agent_xmpp.rb
+++ lib/agent_xmpp.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'xmpp4r'
 require 'eventmachine'
 
+require 'agent_xmpp/parser'
 require 'agent_xmpp/connection'
 require 'agent_xmpp/client'
 require 'agent_xmpp/rexml_addons'
