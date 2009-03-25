b1ea12c49a09d9db2e393487a722a452d8b9d4fb
Troy Stribling <troy.stribling@gmail.com>
2009-03-24 22:31:02 -0400

    version requests are now only sent for available presence messages

--- lib/agent_xmpp/client.rb
+++ lib/agent_xmpp/client.rb
@@ -86,7 +86,7 @@ module AgentXmpp
       if self.roster.has_key?(from_bare_jid) 
         self.roster[from_bare_jid.to_s][:resources][from_jid] = {} if self.roster[from_bare_jid.to_s][:resources][from_jid].nil?
         self.roster[from_bare_jid.to_s][:resources][from_jid][:presence] = presence
-        client_connection.get_client_version(from_jid) unless from_jid.eql?(client_connection.jid.to_s)
+        client_connection.get_client_version(from_jid) if not from_jid.eql?(client_connection.jid.to_s) and presence.type.nil?
         AgentXmpp::logger.info "RECEIVED PRESENCE FROM: #{from_jid}"
       else
         AgentXmpp::logger.warn "RECEIVED PRESENCE FROM JID NOT IN CONTACT LIST: #{from_jid}"        
