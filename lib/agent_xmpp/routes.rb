b01557ffd3c647b2e0c2516338b25cd7a62490ca
Troy Stribling <troy.stribling@gmail.com>
2009-04-16 22:15:36 -0400

    removed default routing. all routes must be specified.

--- lib/agent_xmpp/routes.rb
+++ lib/agent_xmpp/routes.rb
@@ -23,13 +23,15 @@ module AgentXmpp
           route_path = "#{params[:node]}/#{params[:action]}"
           field_path = self.fields(params)
           route_path += "/#{field_path}" unless field_path.nil? 
-          route = self.map[route_path] || {:controller => params[:node], :action => params[:action]}
-          begin
-            controller_class = eval("#{route[:controller].classify}Controller")
-          rescue ArgumentError
-            AgentXmpp::logger.error "ROUTING ERROR: #{params[:node].classify}Controller inavlid for node:#{params[:node]} action:#{params[:action]}."
-          else          
-            controller_class.new.handle_request(connection, route[:action], params)
+          route = self.map[route_path]
+          unless route.nil?
+            begin
+              controller_class = eval("#{route[:controller].classify}Controller")
+            rescue ArgumentError
+              AgentXmpp::logger.error "ROUTING ERROR: #{params[:node].classify}Controller inavlid for node:#{params[:node]} action:#{params[:action]}."
+            else          
+              controller_class.new.handle_request(connection, route[:action], params)
+            end
           end
         end
 
