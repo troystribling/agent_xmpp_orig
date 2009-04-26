f56d797c6f74b88aad4fc9ead466f080405b8257
Troy Stribling <troy.stribling@gmail.com>
2009-04-26 14:32:14 -0400

    more info in README and dealing with issue of execution block result arguments. need to look at what is going on i more detail.

--- app/controllers/system_commands.rb
+++ app/controllers/system_commands.rb
@@ -13,12 +13,12 @@ class SystemCommands
     #.........................................................................................................
     def file_system_usage
       fs_type_result = `df -T`
-      ['hfs', 'ext3', 'ext', 'ext2'].select{|fst| fs_type_result.include?(fst)}.inject([]) do |result, fst|
+      ['hfs', 'ext3', 'ext', 'ext2'].select{|fst| /\s#{fst}\s/.match(fs_type_result)}.inject([]) do |result, fst|
         `df --type=#{fst} -H`.split("\n")[1..-1].each do |row|
           vals = row.split(/\s+/)
           result.push({:mount => vals[5..-1].join(" "), :size => vals[1], :used => vals[4]})
         end
-        result
+        result.count.eql?(1) ? result.first : result
       end
     end
   
